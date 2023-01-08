7a90cf1057cc067d8cb91b75eacaade0144c65d1
Alexis <cucoalexis@hotmail.com>
2023-01-08 13:05:55 +0100

    Día 4

---
+++ 004/v43_01.py
@@ -0,0 +1,15 @@
+states_of_america = ["Delaware", "Pennsylvania"]
+
+print(states_of_america[0])
+print(states_of_america[1])
+print(states_of_america[-1])
+print(states_of_america[-2])
+
+states_of_america[1] = "Pencilvania"
+print(states_of_america)
+
+states_of_america.append("New Jersey")
+print(states_of_america)
+
+states_of_america.extend(["Virginia", "New York"])
+print(states_of_america)

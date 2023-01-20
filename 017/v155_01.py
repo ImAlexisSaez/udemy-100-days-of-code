0915c3e6c50436efd303e6e59a8441ab47d9c443
Alexis <cucoalexis@hotmail.com>
2023-01-20 18:23:24 +0100

    Día 17

---
+++ 017/v155_01.py
@@ -0,0 +1,14 @@
+class User:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+
+user_1 = User("001", "Alexis")
+print(user_1.username)
+print(user_1.followers)
+
+user_2 = User("002", "Ana")
+print(user_2.id)
